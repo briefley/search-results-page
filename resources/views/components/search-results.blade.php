d5a3f89c7566ae3c1756b411b99b551e5dc25363
Lasha Lomidze <samurai@Lashas-MacBook-Pro.local>
2025-01-03 18:02:37 +0400

    Fix up translation typo

--- resources/views/components/search-results.blade.php
+++ resources/views/components/search-results.blade.php
@@ -13,7 +13,7 @@
         <x-filament::breadcrumbs class="!mb-2" :breadcrumbs="[
                 url()->previous() => __('search-results-page::searchresults.back'),
                 __('search-results-page::searchresults.search_results'),
-            ]" />
+            ]"/>
         <div class="flex justify-between items-end">
             <h1 class="fi-header-heading">
                 {{ __('search-results-page::searchresults.search_results') }}
@@ -36,11 +36,11 @@
 
                     <div wire:key="{{ $label }}">
                         <x-search-results-page::result-group
-                            :label="ucwords(str_replace('_', ' ', $label))"
-                            :results="$data"
-                            @class([
-                                'border-b-0 rounded-b-none' => $hasPagination,
-                            ])
+                                :label="ucwords(str_replace('_', ' ', $label))"
+                                :results="$data"
+                                @class([
+                                    'border-b-0 rounded-b-none' => $hasPagination,
+                                ])
                         />
                         @if($hasPagination)
                             <div class="px-5 h-11 flex justify-between items-center text-xs text-custom-gray-70 font-medium bg-table-layout-gray border border-custom-gray-border rounded-b-xl">
@@ -52,19 +52,19 @@
                                 </p>
                                 <div class="flex items-center gap-2.5">
                                     <x-filament::icon-button
-                                        icon="chevron-left"
-                                        class="pagination-button "
-                                        wire:click="previousPage('{{$label}}')"
-                                        :disabled="$page[$label] == 1"
+                                            icon="chevron-left"
+                                            class="pagination-button "
+                                            wire:click="previousPage('{{$label}}')"
+                                            :disabled="$page[$label] == 1"
                                     />
                                     <p>
                                         <span class="text-custom-black">{{ $page[$label]  }}</span>/{{ ceil($categoryTotalRecords / $perPage) }}
                                     </p>
                                     <x-filament::icon-button
-                                        icon="chevron-right"
-                                        class="pagination-button"
-                                        wire:click="nextPage('{{$label}}')"
-                                        :disabled="$page[$label] == ceil($categoryTotalRecords / $perPage)"
+                                            icon="chevron-right"
+                                            class="pagination-button"
+                                            wire:click="nextPage('{{$label}}')"
+                                            :disabled="$page[$label] == ceil($categoryTotalRecords / $perPage)"
                                     />
                                 </div>
                             </div>
