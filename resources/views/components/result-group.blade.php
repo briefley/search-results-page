b768207ea9628d40b278fe3ab5f8cbbfa2643e19
Lasha Lomidze <samurai@Lashas-MacBook-Pro.local>
2025-01-03 17:34:40 +0400

    More configurating and boring namespace fixing

--- resources/views/components/result-group.blade.php
+++ resources/views/components/result-group.blade.php
@@ -7,23 +7,21 @@
     x-data="{ open: true }"
     {{ $attributes->class(['fi-global-search-result-group border border-custom-gray-border rounded-xl overflow-hidden']) }}
 >
-    @php
-        $id = str_replace(' ', '-', strtolower($label));
-        $validIds = array_flip(['brands', 'categories', 'customers', 'orders', 'products', 'shipping-options', 'dynamic-forms', 'discounts']);
-        $isValidId = isset($validIds[$id]);
-    @endphp
 
-    <button @click="open = !open" class="px-6 w-full h-8.5 flex justify-between items-center bg-custom-gray-background border-b border-custom-gray-border transition-all" :class="{ 'border-transparent': !open }">
+    <button @click="open = !open"
+            class="px-6 w-full h-8.5 flex justify-between items-center bg-custom-gray-background border-b border-custom-gray-border transition-all"
+            :class="{ 'border-transparent': !open }">
         <div class="flex items-center gap-2">
             @if ($isValidId)
                 <x-filament::icon
-                    icon="icon-search.{{ $id }}"
+                    icon="icon-search"
                     class="size-3"
                 />
             @endif
             <h3 class="text-sm !text-custom-black font-medium">{{ $label }} ({{ count($results) }})</h3>
         </div>
-        <a href="/admin/{{ \Illuminate\Support\Str::slug($label) }}?tableSearch={{ $this->search }}" class="text-xxs text-custom-gray-90 font-medium hover:underline">
+        <a href="/admin/{{ \Illuminate\Support\Str::slug($label) }}?tableSearch={{ $this->search }}"
+           class="text-xxs text-custom-gray-90 font-medium hover:underline">
             {{ __('::searchresults.view_more_results') }}
         </a>
     </button>
@@ -37,10 +35,6 @@
     >
         @php
             $component =  'search-results-page::result';
-
-            if($isValidId) {
-                $component = 'search-results-page::search-results.results.' . $id;
-            }
         @endphp
 
         @foreach ($results as $result)
